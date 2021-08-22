0f133f9b80581a8a1d7159bbd928588b4d465387
Pramanik, Ankit <pramanik.85849@gmail.com>
2021-08-22 09:59:49 +0530

    Create backend.tf

---
+++ Terraform/remote_terraform_backend/backend.tf
@@ -0,0 +1,20 @@
+#backend for gcp storage
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state-prod"
+    prefix  = "terraform/state"
+  }
+}
+
+
+#backend for aws s3
+
+#terraform {
+#	backend "s3" {
+#		profile = "default"
+#		region = "us-east-1"
+#		key = "terraform.tfstate" #name of the state file
+#		bucket = "bucket_name"
+#	}
+#}
