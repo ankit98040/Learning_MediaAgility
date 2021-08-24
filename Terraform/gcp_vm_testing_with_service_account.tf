6cb2666ec7de8a7e65b84b503e52ee280031c729
Pramanik, Ankit <pramanik.85849@gmail.com>
2021-08-24 13:13:18 +0530

    Create gcp_vm_testing_with_service_account.tf

---
+++ Terraform/gcp_vm_testing_with_service_account.tf
@@ -0,0 +1,49 @@
+provider "google" {
+  project = "playground-s-11-eac1d058"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_service_account" "default" {
+  account_id   = "gcpvmtesting98040"
+  display_name = "Service Account"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  # Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
