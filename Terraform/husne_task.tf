8cabae5a24bc740c8a9c4539f527b665b190a523
Pramanik, Ankit <pramanik.85849@gmail.com>
2021-08-25 09:29:23 +0530

    Update husne_task.tf

--- Terraform/husne_task.tf
+++ Terraform/husne_task.tf
@@ -1,8 +1,14 @@
 provider "google" {
-  project = "playground-s-11-306a48b7"
+  project = "playground-s-11-45ae27d7"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
+
+#not using
+# resource "google_compute_project_metadata_item" "mykey" {
+#   key   = "projectuser"
+#   value = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD1WG9MH3U3UamN72QNioTC0hJL6wbzK/qsF3yNvTdmuVih6tXwGRBTbBY+XvvwYZUicuU3XlkJ7PlJb56KtNXdESO79U2EIZt9gkbJ9SF4riBSzvrVQk3A+AcLuD9/UUHlTSeP+izMlgZnAHdwWSmXoeciB8yE8djPaEDC9n1QGHheJq32bJfzXdqboRU+aVB8OtN0L/Esfno7abKHnTykytGPWjQhYGwsFJ+m/BFs550XHyUIyBO2iRXNro3UOZkadDfhwDS1VYE9lWtD3186j3sYaXiS2c9VMyRzPc5641ReYW4uS7ynUx05/2hTlzDJDmQPI1/DT1bdf8P1q4xtueqvgQSZMze+8w7es//BmNgXmuJG146EvP2imq66tS4A441U5gw0oEO11bb4Fc6ooMjPriD4JCEl7BndhnFTOO6FrKMlztM/eU2TsoM2q60KGYNYJ4IQuJqXkfAJ37cBhACEAuJKnLf7PshcajPLIBgbPODmurMv7ojr3+SxE+hZuNvHRxvt4GQ8hh3fCkHjQtDu48Rsa6yVUwU+h49bvfEmTGF2KA5+lchbXe23jiQWwfIF40HtXWi76dPoiflrLe7CBWVZaLTwCq+aDKFjAQXrpbinD5WKmefC7pvJBXPs9zSaQcCoW6xSWosCvkYmgSO2lw9LDMHv11Cb2uNDLw== projectuser"
+# }
 
 resource "google_compute_project_metadata" "my_ssh_key" {
   metadata = {
@@ -19,15 +25,13 @@ resource "google_compute_project_metadata" "my_ssh_key" {
 #   }
 # }
 
-
-
-resource "google_service_account" "default" {
+resource "google_service_account" "service_account_vm" {
   account_id   = "gcpvmtesting98040"
   display_name = "Service Account"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "test"
+resource "google_compute_instance" "vm_wordpress" {
+  name         = "wordpress"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
 
@@ -36,14 +40,9 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-1804-bionic-v20210817"
     }
   }
-
-  # Local SSD disk
-#  scratch_disk {
-#    interface = "SCSI"
-#  }
 
   network_interface {
     network = "default"
@@ -57,7 +56,58 @@ resource "google_compute_instance" "default" {
 
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
+    email  = google_service_account.service_account_vm.email
     scopes = ["cloud-platform"]
   }
+}
+
+output "wordpressip" {
+    value = google_compute_instance.vm_wordpress.network_interface.0.access_config.0.nat_ip
+}
+
+#passing a file and executing it
+# resource "null_resource" "wordpress_install_next" {
+#     connection {
+#       type = "ssh"
+#       user = "projectuser"
+#       private_key = file("./projectuser")
+#       host = google_compute_instance.vm_wordpress.network_interface.0.access_config.0.nat_ip
+#     }
+
+#     provisioner "file" {
+# 		source = "wp.sh"
+# 		destination = "wp.sh"
+# 	}
+
+#     provisioner "remote-exec" {
+#         inline = [
+#             "sudo chmod +x wp.sh",
+#             "sudo ./wp.sh"
+
+#         ]
+#     }
+  
+# }
+
+resource "null_resource" "wordpress_install_two" {
+    connection {
+      type = "ssh"
+      user = "projectuser"
+      private_key = file("./projectuser")
+      host = google_compute_instance.vm_wordpress.network_interface.0.access_config.0.nat_ip
+    }
+
+    provisioner "file" {
+		source = "wp.sh"
+		destination = "wp.sh"
+	}
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo chmod +x wp.sh",
+            "sudo ./wp.sh"
+
+        ]
+    }
+  
 }
