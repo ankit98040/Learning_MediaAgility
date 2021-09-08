cf643787324d44d935bc5155dc3c955c789ce40b
Pramanik, Ankit <pramanik.85849@gmail.com>
2021-09-08 13:39:52 +0530

    Update network.tf

--- Terraform/Framer Insurance/network.tf
+++ Terraform/Framer Insurance/network.tf
@@ -1,18 +1,17 @@
-resource "google_compute_network" "vpc_network" {
-  project                 = "${google_project.}"
-  name                    = "vpc-network"
+resource "google_compute_network" "testterra" {
+  project                 = "poised-vial-319809"
+  name                    = "testterra"
   auto_create_subnetworks = false
   mtu                     = 1460
   routing_mode = "GLOBAL"
-
+  
 }
 
-#subnet
-resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
-  name          = "test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
+resource "google_compute_subnetwork" "sub1" {
+  name          = "test-sub"
+  ip_cidr_range = "10.2.0.0/26"
   region        = "us-central1"
-  network       = google_compute_network.custom-test.id (vpc_id)
+  network       = google_compute_network.testterra.id
   secondary_ip_range {
     range_name    = "tf-test-secondary-range-update1"
     ip_cidr_range = "192.168.10.0/24"
@@ -24,10 +23,11 @@ resource "google_compute_network" "custom-test" {
   auto_create_subnetworks = false
 }
 
+
 #adding firewall rules
-resource "google_compute_firewall" "default" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
+resource "google_compute_firewall" "ruledef" {
+  name    = "testattack"
+  network = google_compute_network.testterra.id
 
   allow {
     protocol = "icmp"
@@ -38,9 +38,6 @@ resource "google_compute_firewall" "default" {
     ports    = ["80", "8080", "1000-2000"]
   }
 
-  source_tags = ["web"]
+  source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_compute_network" "default" {
-  name = "test-network"
-}
