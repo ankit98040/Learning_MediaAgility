437dc658062d119fc6f0294dff15e8cb3e556eea
Pramanik, Ankit <pramanik.85849@gmail.com>
2021-09-10 09:10:16 +0530

    update

--- Terraform/Insurance/Network/vpc.tf
+++ Terraform/Insurance/Network/vpc.tf
@@ -1,8 +1,8 @@
 #creating vpc with custom
 
 resource "google_compute_network" "testterra" {
-  project                 = "poised-vial-319809"
-  name                    = "testterra"
+  project                 = var.project
+  name                    = var.vpc_name
   auto_create_subnetworks = false
   mtu                     = 1460
   routing_mode = "GLOBAL"
